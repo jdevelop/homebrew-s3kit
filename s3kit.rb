59b7033de0f8be9342bf5091c95c0b557c8b9588
Eugene <jdevelop@users.noreply.github.com>
2020-05-01 22:22:31 -0400

    Version 1.3.0 - tags command

--- s3kit.rb
+++ s3kit.rb
@@ -1,22 +1,22 @@
 class S3kit < Formula
   desc "AWS S3 CLI tooklit"
   homepage "https://github.com/jdevelop/s3kit"
-  url "https://github.com/jdevelop/s3kit/archive/v1.2.0.tar.gz"
-  sha256 "75aaf5657c7b49ae5c5496f0230465c69704c6c51ea4d1c5e4fe5ccf1f86e925"
+  url "https://github.com/jdevelop/s3kit/archive/v1.3.0.tar.gz"
+  sha256 "34df26649f2f5d5444581514f72b6ef5149ea9e35f02d53d7263a0d905a69b65"
 
   depends_on "go" => :build 
   depends_on "upx" => :build 
 
   bottle do
     cellar :any_skip_relocation
-    root_url "https://github.com/jdevelop/s3kit/releases/download/v1.2.0"
-    sha256 "7b6467998341f13a399efefd258313e37e70b67e0f4a9fc573ac2f8ccdacfc72" => :mojave
-    sha256 "7b6467998341f13a399efefd258313e37e70b67e0f4a9fc573ac2f8ccdacfc72" => :high_sierra
-    sha256 "7b6467998341f13a399efefd258313e37e70b67e0f4a9fc573ac2f8ccdacfc72" => :catalina
+    root_url "https://github.com/jdevelop/s3kit/releases/download/v1.3.0"
+    sha256 "8331b4387c58a722f63ed2d3fea51894e04ad69bb124436f6b34a65107d39e40" => :catalina
+    sha256 "8331b4387c58a722f63ed2d3fea51894e04ad69bb124436f6b34a65107d39e40" => :mojave
+    sha256 "8331b4387c58a722f63ed2d3fea51894e04ad69bb124436f6b34a65107d39e40" => :high_sierra
   end
 
   def install
-    system "go", "build","-ldflags", "-s -w", "-trimpath"
+    system "go", "build", "-trimpath", "-ldflags", "-s -w"
     system "upx", "-9", "s3kit"
     bin.install "s3kit"
   end
